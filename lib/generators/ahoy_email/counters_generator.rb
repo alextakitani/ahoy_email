e4cc764d642ef011a7603d42910117c0d219a0fd
Andrew Kane <andrew@ankane.org>
2021-01-04 03:21:24 -0800

    Updated generator [skip ci]

--- lib/generators/ahoy_email/counters_generator.rb
+++ lib/generators/ahoy_email/counters_generator.rb
@@ -10,6 +10,14 @@ module AhoyEmail
         migration_template "counters.rb", "db/migrate/create_ahoy_counters.rb", migration_version: migration_version
       end
 
+      def append_initializer
+        file = "config/initializers/ahoy_email.rb"
+        contents = File.exist?(file) ? File.read(file) : ""
+        ["AhoyEmail.api = true", "AhoyEmail.subscribers << AhoyEmail::CountSubscriber"].each do |line|
+          append_to_file file, "#{line}\n" unless contents.include?(line)
+        end
+      end
+
       def migration_version
         "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
       end
